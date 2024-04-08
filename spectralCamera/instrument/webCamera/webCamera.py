90f6a3dc21c63b08f409b2aee4602b48b60d5804
Stranik <ondra.stranik@gmail.com>
2024-04-08 15:05:13 +0200

    webCamera

---
+++ spectralCamera/instrument/webCamera/webCamera.py
@@ -0,0 +1,116 @@
+"""
+Camera DeviceModel
+
+Created on Mon Nov 15 12:08:51 2021
+
+@author: ostranik
+"""
+#%%
+
+import os
+import time
+import numpy as np
+import cv2
+from viscope.instrument.base.baseCamera import BaseCamera
+
+class WebCamera(BaseCamera):
+    ''' class to control usb/integrated camera. wrapper for cv2 '''
+    DEFAULTS = {'name': 'webCamera',
+                'exposureTime': 1/32,
+                'nFrames': 1,
+                'cameraIdx': 0}
+
+    def __init__(self, name=DEFAULT['name'],*args, **kwargs):
+        ''' initialisation '''
+
+        super(WebCamera,self).__init__(name=name,*args, **kwargs)
+        
+        # camera parameters
+        self.cameraIdx = kwargs['cameraIdx'] if 'cameraIdx' in kwargs else WebCamera.DEFAULTS['cameraIdx']
+        self.exposureTime = BaseCamera.DEFAULT['exposureTime']
+        self.nFrame = BaseCamera.DEFAULT['nFrame']
+
+        self.cap = None
+        self.frame = None
+       
+        self.height = None
+        self.width = None
+
+    def connect(self):
+        super().connect()
+        self.cap = cv2.VideoCapture(self.cameraIdx)
+        # switch off auto-exposure
+        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE,1) 
+        self.set_exposureTime()
+
+        # get the image size
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+    def disconnect(self):
+        super().connect()
+        self.cap.release()
+
+
+    def __str__(self):
+        return 'OpenCV Camera {}'.format(self.cameraIdx)
+
+    def getLastImage(self):
+        myframe = None
+        for _ in range(self.n_frames):
+            temporary_frame = None
+            while temporary_frame is None:
+                ret, temporary_frame = self.cap.read()
+                time.sleep(0.03)
+
+            if myframe is None:
+                myshape = np.shape(temporary_frame.T)
+                myframe = np.reshape(temporary_frame.T, (myshape[0]*myshape[1], myshape[2]))
+                myframe = myframe.astype('int64').T
+            else:
+                myframe = myframe + np.reshape(temporary_frame.T, (myshape[0]*myshape[1], myshape[2])).T
+        self.rawImage = myframe/self.nFrame
+        return self.rawImage
+
+    def _setExposureTime(self,value):
+        self.cap.set(cv2.CAP_PROP_EXPOSURE, value/1000)
+        self.exposureTime = value
+
+    def _getExposureTime(self):
+        _exposureTime = self.cap.get(cv2.CAP_PROP_EXPOSURE)
+        if _exposureTime >0:
+            self.exposureTime = self.cap.get(cv2.CAP_PROP_EXPOSURE)
+        return self.exposureTime
+
+    def _setParameterOpenCV(self, parameter=None, value=None):
+        ''' open cv parameters
+        https://vovkos.github.io/doxyrest-showcase/opencv/sphinx_rtd_theme/enum_cv_VideoCaptureProperties.html
+        '''
+        if (parameter <40) and (value is not None):
+            self.cap.set(parameter, value)
+
+    def _getParameterOpenCV(self, parameter):
+        ''' open cv parameters
+        https://vovkos.github.io/doxyrest-showcase/opencv/sphinx_rtd_theme/enum_cv_VideoCaptureProperties.html
+        '''
+        if (parameter <40):
+            return self.cap.get(parameter)
+        else:
+            return None
+
+
+#%%
+
+if __name__ == '__main__':
+    cam = webCamera()
+    cam.prepareCamera()
+    cam.setParameter('exposureTime',300)
+    cam.setParameter('n_frames', 5)
+
+    cam.startAcquisition()
+    cam.displayStreamOfImages()
+    cam.stopAcquisition()
+    cam.closeCamera()
+
+
+# %%
--- tests/test_gui.py
+++ tests/test_gui.py
@@ -3,7 +3,15 @@
 import pytest
 
 @pytest.mark.GUI
-def test_SpectralCameraGUI():
+def test_webCamera():
     ''' check if gui works'''
-    pass
+    from spectralCamera.instrument.webCamera.webCamera import WebCamera
+
+    cam = WebCamera()
+    cam.connect()
+    cam.setParameter('exposureTime',300)
+    cam.setParameter('nFrames', 5)
+
+    cam._displayStreamOfImages()
+    cam.disconnect()
 
